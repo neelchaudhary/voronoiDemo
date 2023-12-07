b6540b1a7dffd6e65651d9efd264c83db4775092
neelchaudhary <neelchaudhari51097@gmail.com>
2023-12-06 20:10:51 -0500

    Testing

--- Demo.py
+++ Demo.py
@@ -45,7 +45,7 @@ class MainWindow:
             lines = vp.get_output()
             self.drawLinesOnCanvas(lines)
             
-            print lines
+            print(lines)
 
     def onClickClear(self):
         self.LOCK_FLAG = False
